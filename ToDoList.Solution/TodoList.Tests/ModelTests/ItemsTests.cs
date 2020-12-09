943702b820332c9cff13dbfc524edfa503769da7
Danielle Thompson <danithompson74@gmail.com>
2020-12-09 14:06:01 -0800

    All 5 tests pass with GetAll test added
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- ToDoList.Solution/TodoList.Tests/ModelTests/ItemsTests.cs
+++ ToDoList.Solution/TodoList.Tests/ModelTests/ItemsTests.cs
@@ -65,6 +65,20 @@ namespace ToDoList.Test
     }
 
     [TestMethod]
-    
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
