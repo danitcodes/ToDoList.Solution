ae06796953e44db7d839cfe2aede83da3ea3eb9f
Danielle Thompson <danithompson74@gmail.com>
2020-12-09 16:03:57 -0800

    Correctly pass add/view options to console
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- ToDoList.Solution/ToDoList/Program.cs
+++ ToDoList.Solution/ToDoList/Program.cs
@@ -12,16 +12,24 @@ public class Program
     Console.WriteLine("Welcome to the To Do List.");
     Console.WriteLine("--------------------------");
     Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-    string response = Console.ReadLine();
-    Console.WriteLine(response);
+    string response = Console.ReadLine().ToLower();
+    
 
-  //   if (response = "Add" ) {
-  //     List<Item> result = Item.GetAll();  //returns the static list; would have to create new instances of list if it were an instance
-  //     Console.WriteLine("Please enter new list item description: ");
-  //     string newListItem = Console.ReadLine();
-  //     Console.WriteLine(newListItem);
-  //   } else {
-  //     return "false";
-  //   }
+    if (response == "add") {
+      List<Item> result = Item.GetAll();  //returns the static list; would have to create new instances of list if it were an instance
+      Console.WriteLine("Please enter new list item description: ");
+      string newListItem = Console.ReadLine();
+      Item newItem = new Item(newListItem);
+      Program.Main();
+    } 
+    else if (response == "view")
+    {
+      Item.GetAll().ForEach(i => Console.WriteLine(i.Description));
+      Program.Main();
+    }
+    else {
+      Console.WriteLine("false");
+    }
   }
 }
+
