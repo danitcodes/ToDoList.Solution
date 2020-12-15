496e46115ba29507844803a0fde51cef3c9eab38
Danielle Thompson <danithompson74@gmail.com>
2020-12-14 22:50:06 -0800

    Create ItemsController, additional views folder & files, add to list fxnlty

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,35 +1,52 @@
-using System;  // cnct to ... ?
-using System.Collections.Generic; //cnct to built-in items
-using ToDoList.Models;  //cnct to biz logic
+// using System;  // cnct to Console lines
+// using System.Collections.Generic; //cnct to built-in items
+// using ToDoList.Models;  //cnct to biz logic
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-//Add()
-//View()
-
-public class Program 
+namespace ToDoList
 {
-  public static void Main()
+  public class Program
   {
-    Console.WriteLine("Welcome to the To Do List.");
-    Console.WriteLine("--------------------------");
-    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-    string response = Console.ReadLine().ToLower();
-    
-
-    if (response == "add") {
-      List<Item> result = Item.GetAll();  //returns the static list; would have to create new instances of list if it were an instance
-      Console.WriteLine("Please enter new list item description: ");
-      string newListItem = Console.ReadLine();
-      Item newItem = new Item(newListItem);
-      Program.Main();
-    } 
-    else if (response == "view")
+    public static void Main(string[] args)
     {
-      //add inner if/else statement for when user asks to view list before adding items to it
-      Item.GetAll().ForEach(i => Console.WriteLine(i.Description));
-      Program.Main();
-    }
-    else {
-      Console.WriteLine("false");
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
+
+// public class Program 
+// {
+//   public static void Main()
+//   {
+//     Console.WriteLine("Welcome to the To Do List.");
+//     Console.WriteLine("--------------------------");
+//     Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+//     string response = Console.ReadLine().ToLower();
+    
+
+//     if (response == "add") {
+//       List<Item> result = Item.GetAll();  //returns the static list; would have to create new instances of list if it were an instance
+//       Console.WriteLine("Please enter new list item description: ");
+//       string newListItem = Console.ReadLine();
+//       Item newItem = new Item(newListItem);
+//       Program.Main();
+//     } 
+//     else if (response == "view")
+//     {
+//       //add inner if/else statement for when user asks to view list before adding items to it
+//       Item.GetAll().ForEach(i => Console.WriteLine(i.Description));
+//       Program.Main();
+//     }
+//     else {
+//       Console.WriteLine("false");
+//     }
+//   }
+// }
