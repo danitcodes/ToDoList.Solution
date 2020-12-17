d0275d7006ea2af8825052ce0aae80618ccabfe3
Danielle Thompson <danithompson74@gmail.com>
2020-12-16 21:33:28 -0800

    Update Item Create() route

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -37,5 +37,18 @@ namespace ToDoList.Controllers
         model.Add("items", categoryItems);
         return View(model); //View() can only accept one argument
       } //Displays one specific Category's details
-  }
+
+      [HttpPost("/categories/{categoryId}/items")]
+        public ActionResult Create(int categoryId, string itemDescription)
+        {
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Category foundCategory = Category.Find(categoryId);
+          Item newItem = new Item(itemDescription);
+          foundCategory.AddItem(newItem);
+          List<Item> categoryItems = foundCategory.Items;
+          model.Add("items", categoryItems);
+          model.Add("category", foundCategory);
+          return View("Show", model);
+        } //Creates new Item w/in specific Category on server, not new Categories
+      }
 }
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -14,13 +14,6 @@ namespace ToDoList.Controllers
       return View(category);
     } //offers form to create new Item w/in specific Category
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    } //Creates new Item w/in specific Category on server
-
     [HttpGet("/categories/{categoryId}/items/{itemId}")]
     public ActionResult Show(int categoryId, int itemId)
     {
