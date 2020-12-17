d0ea199a734421f811414459d9829a315749fc22
Danielle Thompson <danithompson74@gmail.com>
2020-12-16 20:54:29 -0800

    Add show route w/ Dictionary & add category/show page

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -26,5 +26,16 @@ namespace ToDoList.Controllers
         Category newCategory = new Category(categoryName);
         return RedirectToAction("Index");
       }
+
+      [HttpGet("/categories/{id}")]
+      public ActionResult Show(int id)
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category selectedCategory = Category.Find(id);
+        List<Item> categoryItems = selectedCategory.Items;
+        model.Add("category", selectedCategory);
+        model.Add("items", categoryItems);
+        return View(model); //View() can only accept one argument
+      }
   }
 }
