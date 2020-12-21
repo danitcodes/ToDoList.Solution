c330972651dd9ee88d83ee7e61f3a34229d93b59
Danielle Thompson <danithompson74@gmail.com>
2020-12-20 18:26:38 -0800

    Update Dispose() test & add ItemTests(), comment out old tests

--- ToDoList.Tests/ModelTests/ItemsTests.cs
+++ ToDoList.Tests/ModelTests/ItemsTests.cs
@@ -1,3 +1,4 @@
+using MySql.Data.MySqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;  //this connects to namespace in Item.cs
 using System.Collections.Generic;
@@ -14,75 +15,80 @@ namespace ToDoList.Test
       Item.ClearAll();
     }
 
-    [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    public ItemTests()
     {
-      Item newItem = new Item("test");  //would be in regular method code
-      Assert.AreEqual(typeof(Item), newItem.GetType());
-      //^ typeof(Item) = test on instance of Item
-      //^^ newItem.GetType() = regular method code being tested
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
     }
 
-    [TestMethod]
-    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
-    {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
+  //   [TestMethod]
+  //   public void ItemConstructor_CreatesInstanceOfItem_Item()
+  //   {
+  //     Item newItem = new Item("test");  //would be in regular method code
+  //     Assert.AreEqual(typeof(Item), newItem.GetType());
+  //     //^ typeof(Item) = test on instance of Item
+  //     //^^ newItem.GetType() = regular method code being tested
+  //   }
 
-      //Act
-      int result = newItem.Id;
+  //   [TestMethod]
+  //   public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+  //   {
+  //     //Arrange
+  //     string description = "Walk the dog.";
+  //     Item newItem = new Item(description);
 
-      //Assert
-      Assert.AreEqual(1, result);
-    }
+  //     //Act
+  //     int result = newItem.Id;
 
-    [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+  //     //Assert
+  //     Assert.AreEqual(1, result);
+  //   }
 
-      //Act
-      Item result = Item.Find(2);
+  //   [TestMethod]
+  //   public void Find_ReturnsCorrectItem_Item()
+  //   {
+  //     //Arrange
+  //     string description01 = "Walk the dog";
+  //     string description02 = "Wash the dishes";
+  //     Item newItem1 = new Item(description01);
+  //     Item newItem2 = new Item(description02);
 
-      //Assert
-      Assert.AreEqual(newItem2, result);
-    }
+  //     //Act
+  //     Item result = Item.Find(2);
 
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";//creates instance
-      Item newItem = new Item(description); // pass new instance through Item to add to list
-      //Act
-      string result = newItem.Description; //calls on new item added to list
-      //Assert
-      Assert.AreEqual(description, result); //tests that expected result matches description of new item
-    }
+  //     //Assert
+  //     Assert.AreEqual(newItem2, result);
+  //   }
+
+  //   [TestMethod]
+  //   public void GetDescription_ReturnsDescription_String()
+  //   {
+  //     //Arrange
+  //     string description = "Walk the dog.";//creates instance
+  //     Item newItem = new Item(description); // pass new instance through Item to add to list
+  //     //Act
+  //     string result = newItem.Description; //calls on new item added to list
+  //     //Assert
+  //     Assert.AreEqual(description, result); //tests that expected result matches description of new item
+  //   }
     
+  //   [TestMethod]
+  //   public void SetDescription_SetDescription_String()
+  //   {
+  //     //Arrange
+  //     string description = "Walk the dog.";
+  //     Item newItem = new Item(description);
+
+  //     //Act
+  //     string updatedDescription = "Do the dishes";
+  //     newItem.Description = updatedDescription;
+  //     string result = newItem.Description;
+
+  //     //Assert
+  //     Assert.AreEqual(updatedDescription, result);
+  //   }
+
     [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-
-      //Act
-      string updatedDescription = "Do the dishes";
-      newItem.Description = updatedDescription;
-      string result = newItem.Description;
-
-      //Assert
-      Assert.AreEqual(updatedDescription, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
+    public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
     {
       // Arrange
       List<Item> newList = new List<Item> { };
@@ -94,21 +100,32 @@ namespace ToDoList.Test
       CollectionAssert.AreEqual(newList, result);
     }
 
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
+    // [TestMethod]
+    // public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    // {
+    //   //Arrange, Act
+    //   Item firstItem = new Item("Mow the lawn");
+    //   Item secondItem = new Item("Mow the lawn");
 
-      //Act
-      List<Item> result = Item.GetAll();
+    //   //Assert
+    //   Assert.AreEqual(firstItem, secondItem);
+    // }
+
+  //   [TestMethod]
+  //   public void GetAll_ReturnsItems_ItemList()
+  //   {
+  //     //Arrange
+  //     string description01 = "Walk the dog";
+  //     string description02 = "Wash the dishes";
+  //     Item newItem1 = new Item(description01);
+  //     Item newItem2 = new Item(description02);
+  //     List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+  //     //Act
+  //     List<Item> result = Item.GetAll();
       
-      //Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
+  //     //Assert
+  //     CollectionAssert.AreEqual(newList, result);
+  //   }
   }
 }
--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
@@ -6,29 +7,58 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }  //constructor more or less
     public int Id { get; }  //readonly property - don't ever manually edit b/c could make Ids not unique
-    private static List<Item> _instances = new List<Item> {}; //static is a variable that refers to entire class of Items 
     public Item(string description) // description is argument of new instance
     {
       Description = description;
       //^ Constructor = user argument, & sets lwrCase 'description's home location - "I live here now"
-      _instances.Add(this);
-      // `this` refers to object being actively created - Add `this` to overarching _instances variable
-      Id = _instances.Count;
-      // set unique ID for each new list Item instantiated
-      // do so *after* adding Items to the _instances list in order to get an updated Count for Id
+      }
+    
+    public Item(string description, int id) //overload constructor
+    {
+      Description = description;
+      Id = id;
     }
     public static List<Item> GetAll()
-    {
-      return _instances;
+    { // boilerplate to demonstrate what's under the hood of a .NET app interacting w/ a database; will be refactored again w/ Entity
+      List<Item> allItems = new List<Item>{ }; //makes new list
+      MySqlConnection conn = DB.Connection(); //MySqlConnection object
+      conn.Open(); //line above & current opens a new db cnxn
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand; //query needs to be stored into a MySqlCommand object w/ createCommand() method on the conn object; using as creates an expression that casts cmd into a MySqlCommand object
+      cmd.CommandText = @"SELECT * FROM items;";//above & current constructs our SQL query once our cnxn is open; this line is the SQL command; CommandTest property of MySqlCommand object; sets a property value
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader; // resp. for reading data returned by db in response to the above query command; cast rdr type for use w/ MySQl - rdr is the Data Reader (reps actual reading of db) and `as` casts it to MySqlDataReader object
+      while (rdr.Read()) //built in method that returns a boolean while it goes through db one at a time, advancing to next record; while loop ends if false
+      { // takes ea. indiv record from db and translates into an Item object
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close(); // built-in method; important to close db cnxn when done
+      if (conn != null) // conditional if our db cnxn fails to close properly
+      {
+        conn.Dispose(); // only runs if conn is not null
+      }
+      return allItems;
     }
     public static void ClearAll()
     {
-      _instances.Clear();
+      MySqlConnection conn = DB.Connection(); //create conn obj w/ DB.Connection() method
+      conn.Open(); //opens the cnxn
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand; //MySqlCommand object
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery(); //SQL statements that modify data are executed
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1];
+      //temp placeholder item
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
   }
 }
