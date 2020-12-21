8979aa5989252fa77baf18106d03fe871241aab9
Danielle Thompson <danithompson74@gmail.com>
2020-12-20 19:44:14 -0800

    Update categories controller to save newitems upon creation

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -44,6 +44,7 @@ namespace ToDoList.Controllers
           Dictionary<string, object> model = new Dictionary<string, object>();
           Category foundCategory = Category.Find(categoryId);
           Item newItem = new Item(itemDescription);
+          newItem.Save();
           foundCategory.Add(newItem);
           List<Item> categoryItems = foundCategory.Items;
           model.Add("items", categoryItems);
