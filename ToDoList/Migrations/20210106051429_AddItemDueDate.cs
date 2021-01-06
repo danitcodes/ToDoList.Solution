17ce768625114b1ead6ece22661ce11f56828f26
Danielle Thompson <danithompson74@gmail.com>
2021-01-05 22:16:22 -0800

    Update migrations w/ Items DueDate & Completed

---
+++ ToDoList/Migrations/20210106051429_AddItemDueDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class AddItemDueDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Items",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Items");
+        }
+    }
+}
