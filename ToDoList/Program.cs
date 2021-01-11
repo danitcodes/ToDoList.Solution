c50be9fdda9cfe015d44278987e0da8f7cdc434d
Danielle Thompson <danithompson74@gmail.com>
2021-01-10 21:30:44 -0800

    Remove commented section

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -18,32 +18,3 @@ namespace ToDoList
     }
   }
 }
-
-// public class Program 
-// {
-//   public static void Main()
-//   {
-//     Console.WriteLine("Welcome to the To Do List.");
-//     Console.WriteLine("--------------------------");
-//     Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-//     string response = Console.ReadLine().ToLower();
-    
-
-//     if (response == "add") {
-//       List<Item> result = Item.GetAll();  //returns the static list; would have to create new instances of list if it were an instance
-//       Console.WriteLine("Please enter new list item description: ");
-//       string newListItem = Console.ReadLine();
-//       Item newItem = new Item(newListItem);
-//       Program.Main();
-//     } 
-//     else if (response == "view")
-//     {
-//       //add inner if/else statement for when user asks to view list before adding items to it
-//       Item.GetAll().ForEach(i => Console.WriteLine(i.Description));
-//       Program.Main();
-//     }
-//     else {
-//       Console.WriteLine("false");
-//     }
-//   }
-// }
